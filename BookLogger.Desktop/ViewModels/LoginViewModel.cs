f0b6bf6ca8ad7be53a6ae8f25230b2400c048930
decisivechicken <aarodrigues715@gmail.com>
2025-10-07 21:03:57 -0400

    Added input via GUI

--- BookLogger.Desktop/ViewModels/LoginViewModel.cs
+++ BookLogger.Desktop/ViewModels/LoginViewModel.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Input;
 
 namespace BookLogger.Desktop.ViewModels
 {
-    internal class LoginViewModel
+    public class LoginViewModel
     {
+        public string Username { get; set; } = "";
+        public string Password { get; set; } = "";
+
+        public ICommand LoginCommand { get; }
+        public ICommand RegisterCommand { get; }
+
+        public LoginViewModel()
+        {
+            // Login button: currently shows the entered username/password
+            LoginCommand = new RelayCommand(_ =>
+            {
+                System.Windows.MessageBox.Show(
+                    $"Username: {Username}\nPassword: {Password}",
+                    "Login Attempt"
+                );
+            });
+
+            // Register button: just shows a test message
+            RegisterCommand = new RelayCommand(_ =>
+            {
+                System.Windows.MessageBox.Show(
+                    "Attempted to register!",
+                    "Register"
+                );
+            });
+        }
     }
 }
--- BookLogger.Desktop/Views/LoginView.xaml.cs
+++ BookLogger.Desktop/Views/LoginView.xaml.cs
@@ -1,28 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
+using BookLogger.Desktop.ViewModels;
 
 namespace BookLogger.Desktop.Views
 {
-    /// <summary>
-    /// Interaction logic for LoginView.xaml
-    /// </summary>
     public partial class LoginView : Page
     {
         public LoginView()
         {
             InitializeComponent();
+            DataContext = new LoginViewModel();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (DataContext is LoginViewModel vm && sender is PasswordBox pb)
+            {
+                vm.Password = pb.Password;
+            }
         }
     }
 }
