7784cb1cdf5ceb004fcb9a04e4f723918e7168e4
decisivechicken <aarodrigues715@gmail.com>
2025-10-10 11:02:40 -0400

    Bug fixes + implemented book selection

--- BookLogger.Desktop/Views/DashboardView.xaml.cs
+++ BookLogger.Desktop/Views/DashboardView.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using BookLogger.Data;
 using BookLogger.Data.Models;
 using BookLogger.Desktop.ViewModels;
+using System.DirectoryServices;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -23,26 +25,11 @@ namespace BookLogger.Desktop.Views
 
         private void BookResult_DoubleClick(object sender, MouseButtonEventArgs e)
         {
-            if (DataContext is not DashboardViewModel vm)
-                return;
-
-            var selectedBook = vm.SelectedBook;
-            if (selectedBook == null)
-                return;
-
-            // Navigate to BookDetailsPage
-            var detailsPage = new BookDetailsPage(selectedBook);
-
-            // Replace right-hand content with details page
-            if (this.Parent is Grid parentGrid)
+            if (sender is ListViewItem item && item.Content != null)
             {
-                var rightContentGrid = parentGrid.FindName("RightContentGrid") as Grid;
-                if (rightContentGrid != null)
-                {
-                    rightContentGrid.Children.Clear();
-                    rightContentGrid.Children.Add(detailsPage);
-                }
+                MessageBox.Show("Book selected!");
             }
         }
+
     }
 }
